a7d7f5679151729f17d417e1ee1010e043da6a6b
deepeshpanwar <151609749+deepeshpanwar@users.noreply.github.com>
2023-12-06 17:53:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("Script running..")
+document.querySelector('.cross').style.display='none';
+document.querySelector('.hamburger').addEventListener("click",()=>{
+document.querySelector('.sidebargo').classList.toggle('sidebargo');
+if(document.querySelector('.sidebar').classList.contains('sidebargo')){
+document.querySelector('.ham').style.display='inline'
+document.querySelector('.cross').style.display='none'
+
+
+}
+
+else{
+document.querySelector('.ham').style.display='none'
+document.querySelector('.cross').style.display='inline'
+
+
+
+}
+
+
+                              
+})
